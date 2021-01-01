b850beb7d7462e345135ab21bb9e13643717a09d
Ehsan Samavati <samavati.ehsan@yahoo.com>
2021-01-01 19:03:51 +0000

    get root and size implemented

--- src/tree/binary-search-tree/BinarySearchTree.ts
+++ src/tree/binary-search-tree/BinarySearchTree.ts
@@ -3,7 +3,16 @@ import { BinaryTreeNode } from "./BinaryTreeNode";
 export class BinarySearchTree<U> {
     private _root: null | BinaryTreeNode<U> = null;
     private _size: number = 0;
-    constructor(private _comparator: (a: any, b: any) => number) { }
+
+    constructor(private _comparator: (a: U, b: U) => number, private key?: keyof U) { }
+
+    get root() {
+        return this._root;
+    }
+
+    get size() {
+        return this._size;
+    }
 
     insert(value: U): void {
 
@@ -16,14 +25,41 @@ export class BinarySearchTree<U> {
         }
         let current = this._root;
         while (true) {
-            if (this._comparator(value, current) > 0) {
-
-            } else if (this._comparator(value, current) < 0) {
-
+            if (this._comparator(value, current.value) > 0) {
+                if (!current.right) {
+                    current.right = newNode;
+                    this._size++;
+                    return
+                } else {
+                    current = current.right
+                }
+            } else if (this._comparator(value, current.value) < 0) {
+                if (!current.left) {
+                    current.left = newNode;
+                    this._size++;
+                    return
+                } else {
+                    current = current.left
+                }
             } else {
                 // ignore when nodes are same
                 return;
             }
         }
     }
+
+    find(value: U) {
+        let current = this._root;
+        while (current) {
+            if (this._comparator(value, current.value) < 0) {
+                current = current.left;
+            } else if (this._comparator(value, current.value) > 0) {
+                current = current.right;
+            } else {
+                return current.value;
+            }
+        }
+        return -1;
+    }
+
 }
