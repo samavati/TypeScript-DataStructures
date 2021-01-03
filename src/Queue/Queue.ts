6bcfa8cfe877c8e23b75de69d89f99a8250bd078
Ehsan Samavati <samaavaati@gmail.com>
2021-01-03 20:14:30 +0330

    Queue implemented

---
+++ src/Queue/Queue.ts
@@ -0,0 +1,46 @@
+class Node {
+    constructor(public value: any, public next: null | Node = null) { }
+}
+
+export class Queue {
+
+    private _first: null | Node = null;
+    private _last: null | Node = null;
+    private _size = 0;
+    constructor() { }
+
+    enqueue(value: any) {
+        // Add to the end
+        const newNode = new Node(value);
+        if (!this._last) {
+            this._first = newNode;
+            this._last = newNode;
+        } else {
+            this._last.next = newNode;
+            this._last = newNode;
+        }
+
+        this._size++;
+    }
+
+    dequeue() {
+
+        // get from begining
+        if (!this._first) {
+            return null;
+        }
+
+        let current = this._first;
+
+        if (this._first === this._last) {
+            this._last = null;
+        }
+
+        this._first = this._first.next;
+        current.next = null;
+        this._size--;
+
+        return current.value;
+    }
+
+}
